f8ca465a269570ae27aad7ae4b1df4b6880710a0
Umalkeir <126637917+Umalkeir@users.noreply.github.com>
2023-03-25 15:58:22 -0500

    message

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function  writeCards(names, event){
+    const messages =[];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages
+    
+}
+function countDown(n){
+    for (let i = n; i > -1; i--){
+        console.log(i);   
+    }
+}
+
+
+
+
+
+
+
